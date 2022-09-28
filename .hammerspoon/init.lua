7523ce2dd0d3fb3a22c83b96c58a8c40db2f1ace
Elliott Shugerman <elliott.shugerman@immuta.com>
2022-09-28 19:24:04 -0400

    hammerspoon | attempt and fail bind cmd to spotlight

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,40 @@
-hs.hotkey.bind({"cmd", "shift"}, "T", function()
+hs.hotkey.bind({'cmd', 'shift'}, 'T', function()
     hs.osascript.applescript('tell app "System Events" to tell appearance preferences to set dark mode to not dark mode')
 end)
+
+-- launch spotlight with just cmd
+-- wip, based on https://github.com/Hammerspoon/hammerspoon/issues/1039#issuecomment-253374355
+-- local module = {}
+
+-- module.eventwatcher1 = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(e)
+--     local flags = e:getFlags()
+
+--     if flags.cmd and not (flags.alt or flags.shift or flags.ctrl or flags.fn) then
+--       module.cmdWasPressed = true
+--       module.cmdShouldBeIgnored = false
+--       return false;
+--     end
+
+--     if not flags.cmd then
+--       if module.cmdWasPressed and not module.cmdShouldBeIgnored then
+--         hs.eventtap.keyStroke({'cmd'}, 'space')
+--       end
+--       module.cmdWasPressed = false
+--       module.cmdShouldBeIgnored = false
+--     end
+
+--     return false;
+-- end):start()
+
+-- module.eventwatcher2 = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(e)
+--     local flags = e:getFlags()
+--     print('in watcher 2')
+--     print('flags.cmd', flags.cmd)
+--     print('module.cmdWasPressed', module.cmdWasPressed)
+--     if flags.cmd and module.cmdWasPressed then
+--       print('setting ignore flag...')
+--       module.cmdShouldBeIgnored = true
+--     end
+--     return false;
+-- end):start()
+
