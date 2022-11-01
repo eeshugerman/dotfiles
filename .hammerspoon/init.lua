1629646c5538185120ba4ad3ba30437d9c2560a7
Elliott Shugerman <elliott.shugerman@immuta.com>
2022-11-01 11:48:52 -0400

    hammerspoon | get spotlightCmdModule working properly

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,6 +4,9 @@ end)
 
 hs.hotkey.bind({'cmd', 'shift'}, 'H', function()
     hs.reload()
+    -- neither of these are working?
+    hs.notify.new({title='Hammerspoon', informativeText='Config reloaded'}):send()
+    hs.alert.show('Hammerspoon config reloaded')
 end)
 
 
@@ -20,33 +23,38 @@ end)
 
 -- launch spotlight with just cmd
 -- based on https://github.com/Hammerspoon/hammerspoon/issues/1039#issuecomment-253374355
--- still needs some work
-local spolightCmdModule = {}
+local spotlightCmdModule = {
+  cmdOnlyIsPressed = false,
+  otherModifierWasPressed = false
+}
 
-spolightCmdModule.eventwatcher1 = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(e)
+spotlightCmdModule.eventwatcher1 = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(e)
     local flags = e:getFlags()
 
-    if flags.cmd and not (flags.alt or flags.shift or flags.ctrl or flags.fn) then
-      spolightCmdModule.cmdWasPressed = true
-      spolightCmdModule.cmdShouldBeIgnored = false
+    if flags.cmd and (flags.alt or flags.shift or flags.ctrl or flags.fn) then
+      spotlightCmdModule.otherModifierWasPressed = true
       return false;
     end
 
-    if not flags.cmd then
-      if spolightCmdModule.cmdWasPressed and not spolightCmdModule.cmdShouldBeIgnored then
-        hs.eventtap.keyStroke({'cmd'}, 'space')
-      end
-      spolightCmdModule.cmdWasPressed = false
-      spolightCmdModule.cmdShouldBeIgnored = false
+    if flags.cmd and not (flags.alt or flags.shift or flags.ctrl or flags.fn) then
+      spotlightCmdModule.cmdOnlyIsPressed = true
+      return false;
     end
 
-    return false;
+    if not flags.cmd and not (flags.alt or flags.shift or flags.ctrl or flags.fn) then
+      if spotlightCmdModule.cmdOnlyIsPressed and not spotlightCmdModule.otherModifierWasPressed then
+        hs.eventtap.keyStroke({'cmd'}, 'space')
+      end
+      spotlightCmdModule.cmdOnlyIsPressed = false
+      spotlightCmdModule.otherModifierWasPressed = false
+      return false;
+    end
+
 end):start()
 
-spolightCmdModule.eventwatcher2 = hs.eventtap.new({'all'}, function(e)
-    local flags = e:getFlags()
-    if flags.cmd and spolightCmdModule.cmdWasPressed then
-      spolightCmdModule.cmdShouldBeIgnored = true
+spotlightCmdModule.eventwatcher2 = hs.eventtap.new({'all', hs.eventtap.event.types.flagsChanged}, function(e)
+    if spotlightCmdModule.cmdOnlyIsPressed then
+      spotlightCmdModule.cmdOnlyIsPressed = false
     end
     return false;
 end):start()
