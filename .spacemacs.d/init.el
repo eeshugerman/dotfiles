e5692096b20a025b153707ee027620ee1205aa19
Elliott Shugerman <elliott.shugerman@immuta.com>
2022-12-14 00:24:06 -0500

    spacemacs | whoops, didn't mean to revert this with recent update

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -469,7 +469,7 @@ It should only modify the values of Spacemacs settings."
    ;; If non-nil the frame is undecorated when Emacs starts up. Combine this
    ;; variable with `dotspacemacs-maximized-at-startup' to obtain fullscreen
    ;; without external boxes. Also disables the internal border. (default nil)
-   dotspacemacs-undecorated-at-startup nil
+   dotspacemacs-undecorated-at-startup t ;; this breaks Rectangle stuff in OSX
 
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
