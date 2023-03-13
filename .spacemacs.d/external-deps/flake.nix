a7c62f01a09ae2710ad9a5e2f01fbc9c052ae755
Elliott Shugerman <eeshugerman@gmail.com>
2023-03-12 23:41:41 -0400

    spacemacs | nix | cleaner flake, maybe?
    not sure if this is idiomatic. also it seems to produce a different
    profile layout than the last approach? though i don't see why it would...

--- .spacemacs.d/external-deps/flake.nix
+++ .spacemacs.d/external-deps/flake.nix
@@ -7,24 +7,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-        packages = {
-          # TODO: pyright, ripgrep, gls (on mac)
-
-          sql-formatter = pkgs.nodePackages.sql-formatter;
-          typescript-language-server  = pkgs.nodePackages.typescript-language-server ;
-          vscode-html-languageserver-bin = pkgs.nodePackages.vscode-html-languageserver-bin;
-          # provides html, css, json, eslint (but see note below re: eslint in my/install-external-deps)
-          vscode-langservers-extracted = pkgs.nodePackages.vscode-langservers-extracted;
-          nodejs = pkgs.nodejs; #  not 100% sure this is needed
-          solargraph = pkgs.rubyPackages_3_0.solargraph;
-          ng-template = pkgs.vscode-extensions.angular.ng-template;
-
-          default = pkgs.buildEnv {
-            name = "everything";
-            paths = builtins.attrValues (
-              pkgs.lib.filterAttrs (name: _: name != "default") self.packages.${system}
-            );
-          };
+        packages.default = pkgs.buildEnv {
+          name = "everything";
+          paths = [
+            # TODO: pyright, ripgrep, gls (on mac)
+            pkgs.nodePackages.sql-formatter
+            pkgs.nodePackages.typescript-language-server
+            pkgs.nodePackages.vscode-html-languageserver-bin
+            # provides html, css, json, eslint (but see note below re: eslint in my/install-external-deps)
+            pkgs.nodePackages.vscode-langservers-extracted
+            pkgs.nodejs #  not 100% sure this is neede
+            pkgs.rubyPackages_3_0.solargraph
+            pkgs.vscode-extensions.angular.ng-template
+          ];
         };
       });
 }
