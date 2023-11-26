d971eaebea4b79642cd8c238bafb01ff5cd9055c
Elliott Shugerman <eeshugerman@gmail.com>
2023-11-25 19:11:18 -0500

    emacs | bump flake to nix 23.11

--- .spacemacs.d/nix/flake/flake.nix
+++ .spacemacs.d/nix/flake/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
