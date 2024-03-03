e6c4bb774dd42e5f9f015a57003b08f9238ebb4b
Elliott Shugerman <eeshugerman@gmail.com>
2024-03-03 11:51:46 -0500

    emacs | temp tramp/magit/yadm workaround

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -1493,6 +1493,25 @@ before packages are loaded."
     (magit-status "/yadm::"))
   (spacemacs/set-leader-keys "oy" #'my/magit-yadm)
 
+
+  ;; temp workaround
+  ;; see https://github.com/magit/magit/issues/3624, https://github.com/magit/magit/issues/4720
+  ;; should be able to remove with emacs 29.3 (or if we figure out how to install a newer tramp in the meantime)
+  (defun my--tramp-send-command--workaround-stty-icanon-bug (conn-vec orig-command &rest args)
+    "See: https://github.com/magit/magit/issues/4720"
+    (let ((command
+           (if (string= "stty -icrnl -icanon min 1 time 0" orig-command)
+               "stty -icrnl"
+             orig-command)))
+      (append (list conn-vec command) args)))
+
+  (defun my--tramp-send-command--workaround-stty-icanon-bug--filter-args (args)
+    (apply #'my--tramp-send-command--workaround-stty-icanon-bug args))
+
+  (advice-add 'tramp-send-command :filter-args
+              #'my--tramp-send-command--workaround-stty-icanon-bug--filter-args)
+  ;; end temp workaround
+
   ;; c/c++ ----------------------------------------------------------------------
   (setq c-basic-offset 4)
 
